42fbe3ec37b5771d91c22a505f06550a8efc4606
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-01-17 17:42:12 -0800

    Chapter 1

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = 'b788fd837026f92c549fabd44f137bfa31df0e77c506616268802509772f43da24ca4aff7ab4eac26dea1da668b84c9dbfa7b0e5fa043c0080808507902494d7'
